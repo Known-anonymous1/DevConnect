0954241a84b85e2f9203b9ebd4763648c06f779d
Ankit@rafyLap <apurig22mar2001@gmail.com>
2025-08-23 11:37:48 +0530

    settings, urls, views

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+#URLs for the Project app
+# This file is part of the Project Management System.
+
+from django.urls import path
+from .views import ProjectListCreateView
+
+urlpatterns = [
+    path('projects/', ProjectListCreateView.as_view(), name='project-list-create')
+]
+
+
+#URLs for Transaction model
+
+from .views import ProjectListCreateView, TransactionListCreateView
+
+urlpatterns = [
+    path('projects/', TransactionListCreateView.as_view(), name='project-list-create'),
+    path('transactions/', TransactionListCreateView.as_view(), name='transaction-list-create'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
+#Serializers for the Project model
+# This file is part of the Project Management System.
+
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Project
+from .serializers import ProjectSerializer
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+#Serializers for Transaction model
+
+from .models import Transaction 
+from .serializers import TransactionSerializer
+
+class TransactionListCreateView(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
