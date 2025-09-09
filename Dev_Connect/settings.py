4b529f0250c048ed5caebfc5d852e072320af8f2
Ankit@rafyLap <apurig22mar2001@gmail.com>
2025-09-09 21:37:50 +0530

    Auto commit: changes in 1 files

--- Dev_Connect/settings.py
+++ Dev_Connect/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
-TEMPLATES = [... 'DIRS': [BASE_DIR / "templates"], ...]
+TEMPLATES = [ ... 'DIRS': [BASE_DIR / "templates"], ... ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
