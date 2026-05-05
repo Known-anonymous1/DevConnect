68f151f1aaa82d57031950213fd16addf862a1fe
Known-anonymous1 <apgoswami2001@gmail.com>
2026-05-05 04:01:11 +0000

    fix: clean working foundation - settings, views, urls, migrations

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,27 +1,9 @@
-#URLs for the Project app
-# This file is part of the Project Management System.
-
-from django.urls import path
-from .views import ProjectListCreateView
-
-urlpatterns = [
-    path('projects/', ProjectListCreateView.as_view(), name='project-list-create')
-]
-
-
-#URLs for Transaction model
-
-from .views import ProjectListCreateView, TransactionListCreateView
-
-urlpatterns = [
-    path('projects/', TransactionListCreateView.as_view(), name='project-list-create'),
-    path('transactions/', TransactionListCreateView.as_view(), name='transaction-list-create'),
-]
-
 from django.urls import path
 from .views import (
-    ProjectListCreateView, ProjectRetrieveUpdateDestroyView,
-    TransactionListCreateView, TransactionRetrieveUpdateDestroyView
+    ProjectListCreateView,
+    ProjectRetrieveUpdateDestroyView,
+    TransactionListCreateView,
+    TransactionRetrieveUpdateDestroyView,
 )
 
 urlpatterns = [
