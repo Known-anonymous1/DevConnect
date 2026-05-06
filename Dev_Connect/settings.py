0b93fc9393fce2a39959331f74b8e6ba76a56e85
Known-anonymous1 <apgoswami2001@gmail.com>
2026-05-06 02:41:24 +0000

    Auto commit: changes in 4 files

--- Dev_Connect/settings.py
+++ Dev_Connect/settings.py
@@ -91,3 +91,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
